3fcbc4d3351985e4a8d1f1f3d3a4f2fbe82035a1
J-Koral <jonathan.koral2004@gmail.com>
2026-05-08 14:27:59 -0400

    Update CORS configuration to allow all origins and methods

--- src/main/java/edu/brooklyn/cpusim/configuration/CorsConfig.java
+++ src/main/java/edu/brooklyn/cpusim/configuration/CorsConfig.java
@@ -7,15 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://j-koral.github.io") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
