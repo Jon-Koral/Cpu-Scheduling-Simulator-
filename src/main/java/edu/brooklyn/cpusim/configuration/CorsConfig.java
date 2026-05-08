8b5b9370e3f6b8aeefd0f107a5317a974b1e0685
J-Koral <jonathan.koral2004@gmail.com>
2026-05-08 14:34:02 -0400

    Refactor CORS configuration to use CorsFilter

--- src/main/java/edu/brooklyn/cpusim/configuration/CorsConfig.java
+++ src/main/java/edu/brooklyn/cpusim/configuration/CorsConfig.java
@@ -2,22 +2,21 @@ package edu.brooklyn.cpusim.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://j-koral.github.io");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); 
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
